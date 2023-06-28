0104184a94909e9f49c93f50ff2e18e6c0d1e319
supreeta-anand-byatnal <supreeta.byatnal@gmail.com>
2023-06-28 13:07:06 +0530

    Change with app

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
-import App from "./App";
+import App from "./app";
 import { store } from "./services/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
